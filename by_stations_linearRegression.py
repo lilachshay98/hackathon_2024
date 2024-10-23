53308dc5b913b389cd29f074ecdd566007dee3fd
lilach shay <lilach.shay@mail.huji.ac.il>
2024-10-23 15:58:42 +0300

    changed negatives to zero and made int

--- by_stations_linearRegression.py
+++ by_stations_linearRegression.py
@@ -1,6 +1,5 @@
 import csv
 import math
-import  first_baseline
 from joblib import load
 
 import numpy as np
@@ -8,18 +7,20 @@ from tqdm import tqdm
 import preprocessing
 import pandas as pd
 from datetime import datetime
-pd.options.mode.copy_on_write = True
 from joblib import dump
-
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
+pd.options.mode.copy_on_write = True
+
+
 def eval_boardings(predictions: pd.DataFrame, ground_truth: pd.DataFrame):
     combined = pd.merge(predictions, ground_truth, on='trip_id_unique_station')
     mse_board = mean_squared_error(combined["passengers_up_x"], combined["passengers_up_y"])
     return mse_board
+
 
 def get_df_for_test(df):
     df['time_in_station (sec)'] = 0
@@ -31,16 +32,20 @@ def get_df_for_test(df):
             row["door_closing_time"] = row["arrival_time"]
 
         if not preprocessing.is_time_after(row["door_closing_time"], row["arrival_time"]):
-            df.iloc[index, df.columns.get_loc("door_closing_time")], df.iloc[index, df.columns.get_loc("arrival_time")] = row["arrival_time"], row[
+            df.iloc[index, df.columns.get_loc("door_closing_time")], df.iloc[
+                index, df.columns.get_loc("arrival_time")] = row["arrival_time"], row[
                 "door_closing_time"]
 
         # passenger_cont_is_int_pos
         if row["passengers_continue"] <= 0:
             df.iloc[index, df.columns.get_loc("passengers_continue")] = 0
 
-        df.iloc[index, df.columns.get_loc('time_in_station (sec)')] = preprocessing.time_difference(row["arrival_time"], row["door_closing_time"])
+        df.iloc[index, df.columns.get_loc('time_in_station (sec)')] = preprocessing.time_difference(row["arrival_time"],
+                                                                                                    row[
+                                                                                                        "door_closing_time"])
 
-        return df[['time_in_station (sec)', "passengers_continue", "door_closing_time", 'trip_id_unique_station', "station_id"]]
+        return df[['time_in_station (sec)', "passengers_continue", "door_closing_time", 'trip_id_unique_station',
+                   "station_id"]]
 
 
 if __name__ == '__main__':
@@ -50,7 +55,7 @@ if __name__ == '__main__':
     baseline_model = load('linear_regression_model.joblib')
 
     # Replace 'path_to_file.csv' with the actual path to your CSV file
-    df = pd.read_csv(r'C:\Elchanan\masters\second_year\Semester_B\IML\hackathon\train_bus_schedule_filtered.csv',
+    df = pd.read_csv(r'/Users/lilachshay/IML/hackathon_2024_public/train_bus_schedule_filtered.csv',
                      encoding="utf-8")
 
     X = df[['time_in_station (sec)', 'passengers_continue', 'trip_id_unique_station', "station_id"]]
@@ -72,7 +77,6 @@ if __name__ == '__main__':
 
     # Wrap the list with tqdm for a progress bar
     for key, group in tqdm(X_y_train_grouped):
-
         X_train_model = group[['time_in_station (sec)', 'passengers_continue']]
         y_train_model = group['passengers_up']
 
@@ -85,14 +89,12 @@ if __name__ == '__main__':
         # if i == 5:
         #     break
 
-
     ########## PREDICTIONS ##########################
-    df_test = pd.read_csv(r'C:\Elchanan\masters\second_year\Semester_B\IML\hackathon\HU.BER\X_passengers_up.csv',
-                     encoding="ISO-8859-8")
+    df_test = pd.read_csv(r'/Users/lilachshay/IML/hackathon_2024_public/train_bus_schedule_filtered.csv',
+                          encoding="utf-8")
     X_test = get_df_for_test(df_test)
     # X_test['passengers_up'] = y_test["passengers_up"]
     X_y_test_sorted = X_test.sort_values(by='station_id')
-
 
     # y_train['passengers_up'] = X_train['passengers_up']
     # X_train = X_train.drop('passengers_up', axis=1)
@@ -119,6 +121,12 @@ if __name__ == '__main__':
             model = model_per_stations_dict[key]
             y_station_predict = model.predict(X_test_model)
 
+        # Replace negative values with zero
+        y_station_predict = np.where(y_station_predict < 0, 0, y_station_predict)
+
+        # Round predictions to the nearest integer and ensure they are integers
+        y_station_predict = np.round(y_station_predict).astype(int)  # Round and convert to int
+
         # Create a DataFrame with the predictions
         predictions_df = pd.DataFrame({
             'trip_id_unique_station': group['trip_id_unique_station'],
@@ -137,11 +145,4 @@ if __name__ == '__main__':
 
     # print(f"Mean Squared Error: {mse}")
 
-
-
     # for index, row in tqdm(df.iterrows(), total=df.shape[0]):
-
-
-
-
-
